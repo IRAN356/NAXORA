2e9ddfd2f5579b441b7909860119213c1a598c82
IRAN356 <fdhdhnf5@gmail.com>
2026-07-16 17:05:50 +0330

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,24 @@
+function sendMessage() {
+    let input = document.getElementById("userInput");
+    let message = input.value;
 
+    let chatBox = document.getElementById("chatBox");
+
+    chatBox.innerHTML += "<p>تو: " + message + "</p>";
+
+    let botReply = "";
+
+    if (message.includes("سلام")) {
+        botReply = "سلام 👋";
+    } 
+    else if (message.includes("تو کی هستی")) {
+        botReply = "من NAXORA هستم 🤖";
+    } 
+    else {
+        botReply = "متوجه نشدم 🤔";
+    }
+
+    chatBox.innerHTML += "<p>بات: " + botReply + "</p>";
+
+    input.value = "";
+}
