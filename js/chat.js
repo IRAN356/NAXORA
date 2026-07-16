00f1033213de0d6642db18f2baf29aff1d4b1e44
IRAN356 <fdhdhnf5@gmail.com>
2026-07-16 16:10:27 +0330

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,17 @@
+const input = document.getElementById("messageInput");
+const button = document.getElementById("sendBtn");
 
+button.addEventListener("click", function () {
+    const message = input.value.trim();
+
+    if (message === "") return;
+
+    const chatBox = document.getElementById("chatBox");
+
+    const msg = document.createElement("div");
+    msg.textContent = message;
+
+    chatBox.appendChild(msg);
+
+    input.value = "";
+});
